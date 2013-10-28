62b6e2481e3189eeadcec46ef6718bfd4ba77d73
Cheng-Chian-Lin <sponge41610@hotmail.com>
2013-10-28 13:34:13 +0800

    add mmtest command

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include "fio.h"
 #include "shell.h"
 #include "str_func.h"
+#include "mmtest.h"
 
 
 void shell_printf(char *buf, int count){
@@ -17,12 +18,16 @@ void check_keyword(char *msg, char *msg_size)
 		if (!(strcmp(msg,"help"))) {
 			fio_write(1, "\n\rsupported instructions are as follow:\n\r",41);
 			fio_write(1, "\n\rhello --> greetings from computer!\n\r",38);
+			fio_write(1, "\n\rmmtest --> perform malloc and free!\n\r",43);
 			
 		}
 else if(!(strcmp(msg,"hello"))) {
-fio_write(1, "\n\rHello! How are you doing?",29);
+fio_write(1, "\n\rHello! How are you doing?",28);
 
 }	
+else if(!(strcmp(msg,"mmtest"))) {
+mmtest_command();
+}
 	else{
 		fio_write(1, "\n\rcommand not found!\n\r",22);
 	}
